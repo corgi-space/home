eca65a5811bad342c61821889c19b31f00d17309
xluoyu <llzz0895@163.com>
2023-04-09 21:40:24 +0800

    chore: 项目基础建设

--- packages/root/src/main.ts
+++ packages/root/src/main.ts
@@ -1,10 +1,11 @@
 /*
  * @Description:
  * @Author: xluoyu
- * @LastEditTime: 2023-04-09 18:39:49
+ * @LastEditTime: 2023-04-09 19:34:05
  */
 import { createApp } from 'vue'
 import './style.css'
+import './style/index.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
