a068ca25ae12dcce3a2d3f7d22d595f004b07b58
一只死兔子 <llzz0895@163.com>
2023-07-24 17:38:57 +0800

    暂存

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -4,14 +4,32 @@ import { IAppIcon, defaultList } from "./appList"
 import { useLocalStorageState } from "ahooks"
 import AppItem from "./components/AppItem"
 import { Apps } from "./Apps/index"
+import { useState, useEffect } from "react"
 
 function useLoadApps() {
 	const [loadStatus, setLoadStatus] = useState(false)
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (loadStatus) {
+				clearTimeout(timer)
+			} else {
+				const appLength = Object.keys(Apps).length
+
+				if (appLength) {
+					setLoadStatus(true)
+				}
+			}
+		}, 60)
+
+		return () => clearInterval(timer)
+	}, [])
 
 	return loadStatus
 }
 
 function Home() {
+	const loadStatus = useLoadApps()
 	const [list] = useLocalStorageState<IAppIcon[]>("appList", {
 		defaultValue: defaultList
 	})
@@ -20,7 +38,9 @@ function Home() {
 	return (
 		// <Theme>
 		<div className="grid-container h-full w-full">
-			{list && list.map(item => <AppItem key={item.id} item={item} />)}
+			{loadStatus
+				? list && list.map(item => <AppItem key={item.id} item={item} />)
+				: null}
 		</div>
 		// </Theme>
 	)
