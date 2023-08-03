c080648fe3202ced1909433f46378b7da57ab211
一只死兔子 <llzz0895@163.com>
2023-08-03 10:52:46 +0800

    refactor: 暂时关闭部分无用代码

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -1,8 +1,8 @@
 import { create } from "zustand"
 import { getStorage, setStorage } from "@/utils/storage"
 import type { IPosition, ITheme } from "@/types/index"
-import axios from "axios"
-import { WeatherKey } from "@/config"
+// import axios from "axios"
+// import { WeatherKey } from "@/config"
 
 type State = {
 	theme: ITheme
@@ -46,42 +46,42 @@ updateHtmlTheme(useAppStore.getState().theme)
  * 根据IP获取当前地址
  * @param position
  */
-const savePosition = async (position: string) => {
-	const res = await axios
-		.get(
-			`https://geoapi.qweather.com/v2/city/lookup?location=${position}&key=${WeatherKey}&lang=zh`
-		)
-		.then(res => {
-			const location = res.data.location[0]
+// const savePosition = async (position: string) => {
+// 	const res = await axios
+// 		.get(
+// 			`https://geoapi.qweather.com/v2/city/lookup?location=${position}&key=${WeatherKey}&lang=zh`
+// 		)
+// 		.then(res => {
+// 			const location = res.data.location[0]
 
-			return {
-				name: location.name,
-				city: location.adm2,
-				cityId: location.id
-			}
-		})
-	setStorage({
-		key: "position",
-		data: res,
-		fn: sessionStorage
-	})
-	useAppStore.setState(() => ({ position: res }))
-}
+// 			return {
+// 				name: location.name,
+// 				city: location.adm2,
+// 				cityId: location.id
+// 			}
+// 		})
+// 	setStorage({
+// 		key: "position",
+// 		data: res,
+// 		fn: sessionStorage
+// 	})
+// 	useAppStore.setState(() => ({ position: res }))
+// }
 
-const getPosition = () => {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(
-			position => {
-				savePosition(position.coords.longitude + "," + position.coords.latitude)
-			},
-			error => {
-				console.log(error)
-			}
-		)
-	} else {
-		console.log("no navigator.geolocation")
-	}
-}
+// const getPosition = () => {
+// 	if (navigator.geolocation) {
+// 		navigator.geolocation.getCurrentPosition(
+// 			position => {
+// 				savePosition(position.coords.longitude + "," + position.coords.latitude)
+// 			},
+// 			error => {
+// 				console.log(error)
+// 			}
+// 		)
+// 	} else {
+// 		console.log("no navigator.geolocation")
+// 	}
+// }
 
 // getPosition()
 
