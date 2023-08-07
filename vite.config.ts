0cf19d6453d5095fd934a75f68b2d1867f40713b
一只死兔子 <llzz0895@163.com>
2023-08-07 17:26:34 +0800

    ci: 添加构建插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { resolve } from "node:path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import Icons from "unplugin-icons/vite"
+import { visualizer } from "rollup-plugin-visualizer"
+import legacy from "@vitejs/plugin-legacy"
+import viteCompression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +23,26 @@ export default defineConfig({
 			autoInstall: true,
 			compiler: "jsx",
 			jsx: "react"
+		}),
+		visualizer({
+			emitFile: false,
+			open: true //如果存在本地服务端口，将在打包后自动展示
+		}),
+		legacy(),
+		viteCompression({
+			verbose: true,
+			disable: false, // 不禁⽤压缩
+			deleteOriginFile: false, // 压缩后是否删除原⽂件
+			threshold: 10240, // 压缩前最⼩⽂件⼤⼩
+			algorithm: "gzip", // 压缩算法
+			ext: ".gz" // ⽂件类型
 		})
-	]
+	],
+	build: {
+		terserOptions: {
+			output: {
+				comments: true
+			}
+		}
+	}
 })
